decb37484b20e540e1716d98299a27ffb1d087af
Luis Gómez <luis15gomez@gmail.com>
2021-07-23 16:36:16 -0500

    Update build_llvm.sh

--- LLVM_tools/build_llvm.sh
+++ LLVM_tools/build_llvm.sh
@@ -3,14 +3,14 @@
 # Instructions:
 
 # Install ninja: brew install ninja
-# Clone https://github.com/apple/swift-llvm
+# Clone https://github.com/apple/llvm-project
 # Go to the tag that corresponds to your xcode version
 # Apply these PR if necessary
-https://reviews.llvm.org/D71219 (Requiered if you have Xcode 12.4)
-https://reviews.llvm.org/D7102 (Requiered if you have Xcode 12.4)
-https://reviews.llvm.org/D71217 (Requiered if you have Xcode 12.4)
-https://reviews.llvm.org/D94202 (Requiered if you have Xcode 12.4 or Xcode 12.5.x)
-# Copy this script in swift-llvm project
+# https://reviews.llvm.org/D71219 (Requiered if you have Xcode 12.4)
+# https://reviews.llvm.org/D7102 (Requiered if you have Xcode 12.4)
+# https://reviews.llvm.org/D71217 (Requiered if you have Xcode 12.4)
+# https://reviews.llvm.org/D94202 (Requiered if you have Xcode 12.4 or Xcode 12.5.x)
+# Copy this script in llvm-project project
 # Open the terminal and 'cd' swift-llvm directory
 # From there, run this script with 'sh build_llvm.sh'
 
